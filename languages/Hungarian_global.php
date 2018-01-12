a1aa3263b4c56511436107a5cd38cc461d9ee7fb
Gábor <yesszus@gmail.com>
2018-01-12 11:07:40 +0100

    Add files via upload

---
+++ languages/Hungarian_global.php
@@ -0,0 +1,19 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Revision    1.1
+|        Date        10.04.2017
+|        Author      Jimako ( www.e107.sk )
+|
+|        Last Modified: 2018/01/12 10:37:20
+|
+|        $Author: Yesszus $
++-----------------------------------------------------------------------+
+*/
+define("LAN_CP_NAME", "Kapcsolat Oldal");
+define("LAN_CP_SUMMARY", "Kiterjesztett Kapcsolat Oldal");
+define("LAN_CP_DESCRIPTION", "Kiterjesztett Kapcsolat Oldal");
+?>
